7d2f40fb4ae9f3804d658f3c0384bf99a7bccf42
MohamedFathi <mohamedkdr66@yahoo.com>
2023-07-07 19:31:26 +0300

    add validations and welcome email

--- auth/views.py
+++ auth/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login as adminLogin ,logout as adminLogout
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -19,11 +20,39 @@ def register(request):
         password = request.POST['password']
         password_confirmation = request.POST['password_confirmation']
 
+        if User.objects.filter(username=username) :
+            messages.error(request,"Username has already taken")
+            return redirect('home')
+
+        if User.objects.filter(email=email) :
+            messages.error(request,"Email has already taken")
+            return redirect('home')
+
+        if len(username) > 10 :
+            messages.error(request,"Username must be under 10 chars")
+            return redirect('home')
+
+        if password != password_confirmation :
+            messages.error(request,"passwords doesn't match")
+            return redirect('home')
+
+        if not username.isalnum() :
+            messages.error(request,"Username must be alphanumeric")
+            return redirect('home')
+
         myUser = User.objects.create_user(username,email,password)
         myUser.first_name = name
         myUser.save()
 
         messages.success(request,"Your Account Has Been Successfully Created .")
+
+        # Welcome Email
+        subject = "Welcome To Our Django Login !"
+        message = "Hello " + myUser.first_name  + "!! \n" + "Welcome To GFG !! \n Thank ypu For Visiting Our Website \n We have also send you a confirmation email, please confirm your email address in order to activate your account . \n\n Thanking"
+        from_email = "mohamed@yahoo.com"
+        to_list = [myUser.email]
+        send_mail(subject,message,from_email,to_list,fail_silently=True)
+
 
         return redirect('login')
 
